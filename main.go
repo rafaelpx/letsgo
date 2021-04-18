36caf4a3909fd5cf4716685e9ba0ff979ad149ae
Rafael Soares <rafaelpx@gmail.com>
2021-04-18 21:21:27 +0100

    More practice of the TDD workflow
    Integers, addition
    
    Writing better documentation so users of our code can understand its usage quickly
    
    Examples of how to use our code, which are checked as part of our tests

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/rafaelpx/letsgo/greetings"
+	"github.com/rafaelpx/letsgo/integers"
 )
 
 func main() {
 	fmt.Println(greetings.Hello("Rafa", "French"))
+	fmt.Println(integers.Add(2, 2))
 }
